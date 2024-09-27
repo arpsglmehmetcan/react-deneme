34d4f056ba4fcdf6dc001d974116517163374573
Mehmetcan Arapisaoğlu <mehmetcanarapisaoglu@ehm.local>
2024-09-27 14:25:38 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg"
+function App() {
+  return (
+    <div className="App">
+      Hoşgeldiniz bu benim ilk projem.
+      <br/>
+      <img src="/logo192.png" alt=" " />
+    </div>
+  );
+}
+
+export default App;
