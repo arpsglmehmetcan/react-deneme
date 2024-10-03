a2ebde24505fff4aceef8fe5cdea39a544fa674e
Mehmetcan Arapisaoğlu <mehmetcanarapisaoglu@ehm.local>
2024-10-03 16:58:58 +0300

    react projesinin güncel hâli

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from "./logo.svg"
+import {useEffect} from "react";
+import Test from "./Test"
+import { Title } from "./Component";
+import Bootstrap from "./Bootstrap";
+import Tailwind from "./tailwind";
+
+import './tailwind.css'
+import styles from './App.module.css'
+
 function App() {
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      // analytici bilgilendir
+    }
+  }, [])
+
   return (
-    <div className="App">
-      Hoşgeldiniz bu benim ilk projem.
-      <br/>
-      <img src="/logo192.png" alt=" " />
+    <div className={styles.App}>
+      <Title>{process.env.NODE_ENV}</Title>
+      <Title theme="dark">{process.env.NODE_ENV}</Title>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+      <Test />
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <img src="/logo192.png" alt=""/>
+          <img src={logo} alt=""/>
+        </>
+      )}
+      <Bootstrap />
+      <tailwind />
     </div>
   );
 }
---
+++ src/tailwind.js
@@ -0,0 +1,12 @@
+function tailwind() {
+	return (
+		<div className="bg-red-500">
+			<h3 className="text-xl font-semibold text-gray-400 underline">tailwind Başlık</h3>
+			<button className="flex items-center bg-black text-white/50 hover:!bg-red-600 px-4 rounded">
+				Test
+			</button>
+		</div>
+	)
+}
+
+export default tailwind
