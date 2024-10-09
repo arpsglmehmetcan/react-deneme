60b4de3892d70a8254ed376060813cdb2fde472c
Mehmetcan Arapisaoğlu <mehmetcanarapisaoglu@ehm.local>
2024-10-09 12:06:35 +0300

    Sap ile css kullanımı video 4

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Test from "./Test"
 import { Title } from "./Component";
 import Bootstrap from "./Bootstrap";
 import Tailwind from "./tailwind";
-
-import './tailwind.css'
+import './style.scss'
+//import './tailwind.css'
 import styles from './App.module.css'
 
 function App() {
@@ -20,8 +20,9 @@ function App() {
     <div className={styles.App}>
       <Title>{process.env.NODE_ENV}</Title>
       <Title theme="dark">{process.env.NODE_ENV}</Title>
-      <p>
+      <p className="env">
         {process.env.REACT_APP_API_URL}
+        <span>Test</span>
       </p>
       <Test />
       {process.env.NODE_ENV === 'production' && (
